6a263a5efbfb76af37c47b48f036f28f2b2758cb
Yury Chernysh <yura388@yandex.ru>
2021-02-28 15:22:07 +0300

    first 5 tasks have been complited

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+  if (n != 0 && n != 1){
+    let result = n;
+    while (n > 1){
+      result = result * (n-1);
+      --n;
+    }
+    return result;
+  } else {
+    return 1;
+  }
 }
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,6 @@
 function checkSpam(str) {
-  // ваш код...
+  str = str.toLowerCase();
+  let spam = 'xxx';
+  let spam1 = '1xbet';
+  return str.includes(spam) || str.includes(spam1) 
 }

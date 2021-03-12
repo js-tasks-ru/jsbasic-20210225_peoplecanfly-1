6f8de6e8ef79c061aa0802c02b1ba464ccfd4da1
Yury Chernysh <yura388@yandex.ru>
2021-03-12 12:02:29 +0300

    HW 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,9 +1,9 @@
 function makeFriendsList(friends) {
   let ulUserList = document.createElement('ul');
-  for(let user of friends){
+  friends.forEach(user => {
     let liElement = document.createElement('li');
     liElement.innerText = user.firstName + ' ' + user.lastName;
     ulUserList.appendChild(liElement);
-  }
+  });
   return ulUserList;
 }

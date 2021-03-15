3f4f108669ae2147ccdd81924663053527d3a16b
Yury Chernysh <yura388@yandex.ru>
2021-03-15 23:58:04 +0300

    первая попытка дз5.

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,15 @@
 function toggleText() {
-  // ваш код...
+  const text = document.querySelector("#text");
+  const toggleFunc = function () {
+    !text.hidden ? (text.hidden = true) : (text.hidden = false);
+  };
+
+  document
+    .querySelector(".toggle-text-button")
+    .addEventListener("click", toggleFunc);
+
+  
+
+ 
+
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,27 @@
 function initCarousel() {
-  // ваш код...
+  let iterationQty = 0;
+  const leftButton = document.querySelector(".carousel__arrow_left");
+  const rightButton = document.querySelector(".carousel__arrow_right");
+  const carouselInner = document.querySelector(".carousel__inner");
+
+  // мы уже находимся в крайнем левом положении, по этому кнопку надо сразу скрыть.
+  // Если можно было бы модфицировать верстку я бы выставил по центру тогда бы не надо было кнопку скрывтать.
+  rightButton.style.display = "none";
+
+  let sliderMove = function () {
+    // подумал так лучше что бы через this что бы общая функция для обоих кнопок была.
+    this.classList.contains("carousel__arrow_left")
+      ? iterationQty++
+      : iterationQty--;
+    carouselInner.style.transform = `translateX(-${carouselInner.offsetWidth * iterationQty}px)`;
+    iterationQty == 3
+      ? (leftButton.style.display = "none")
+      : (leftButton.style.display = "");
+    iterationQty == 0
+      ? (rightButton.style.display = "none")
+      : (rightButton.style.display = "");
+  };
+
+  leftButton.addEventListener("click", sliderMove);
+  rightButton.addEventListener("click", sliderMove);
 }

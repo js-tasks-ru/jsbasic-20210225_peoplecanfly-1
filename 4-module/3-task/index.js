0b079e78646616b247af74e0d0e72de89c806756
Yury Chernysh <yura388@yandex.ru>
2021-03-15 09:26:56 +0300

    Убрал создание функции в каждой итерации

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,44 +1,39 @@
 function highlight(table) {
- [...table.querySelectorAll('tr')]
- .forEach((row,index) => {
-    if (index===0){
+  [...table.querySelectorAll("tr")].forEach((row, index) => {
+    if (index === 0) {
       return;
-    } 
-    applyStylesForRow(row)
- })
+    }
+    applyStylesForRow(row);
+  });
 }
- 
+
+const addLineThroughIfNeeded = (row) => {
+  if (+row.cells[1].innerText < 18) {
+    row.style.textDecoration = "line-through";
+  }
+};
+
+const addAgeClassIfNeeded = (row) => {
+  if (row.cells[2].innerText == "m") {
+    row.classList.add("male");
+  } else if (row.cells[2].innerText == "f") {
+    row.classList.add("female");
+  }
+};
+
+const addAvailableStatusClassIfNeeded = (row) => {
+  if (row.cells[3].dataset.available == "true") {
+    row.classList.add("available");
+  } else if (row.cells[3].dataset.available == "false") {
+    row.classList.add("unavailable");
+  } else {
+    row.hidden = true;
+  }
+};
 
 
-//  в каждой строке проходим по каждой ячейке. Так как у каждой ячейки свои проверки.
 const applyStylesForRow = (row) => {
-
-  const addLineThroughIfNeeded = row =>{
-    if (+row.cells[1].innerText < 18){
-      row.style.textDecoration = 'line-through'
-    }
-  }
-  
-  const addAgeClassIfNeeded = row => {
-    if (row.cells[2].innerText == 'm'){
-      row.classList.add('male');
-    } else if (row.cells[2].innerText== 'f'){
-      row.classList.add('female')
-    }
-  }
-  
-  const addAvailableStatusClassIfNeeded = row => {
-    if (row.cells[3].dataset.available == 'true'){
-      row.classList.add('available')
-    }else if (row.cells[3].dataset.available == 'false'){
-      row.classList.add('unavailable')
-    }else {
-      row.hidden = true;
-    }
-  }
-
   addLineThroughIfNeeded(row);
   addAgeClassIfNeeded(row);
   addAvailableStatusClassIfNeeded(row);
-}
-
+};

2d6ab181e4689a34fc39db08b652a9229ce2d6b7
Yury Chernysh <yura388@yandex.ru>
2021-03-21 14:43:45 +0300

    ДЗ6 частично

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,60 @@
  *
  */
 export default class UserTable {
+  tableCoreStructure = ` 
+  <thead>
+    <tr>
+      <th>Имя</th>
+      <th>Возраст</th>
+      <th>Зарплата</th>
+      <th>Город</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+  </tbody>`; // корневая структура таблицы
+  elem; // элемент дома который потом будет добавлен в html.body.
+
   constructor(rows) {
+    this.table = rows;
+    this.#setDomElement(); // Функция  которая присваивает финальный ДОМ элемент this.elem
+  }
+
+  // Основная функция которая приваивает свойсту класса elem - готовый DOM элемент таблицы.
+  #setDomElement() {
+    const domStructure = this.#insertCoreStructureToElem();
+    let allRowsTemaplate = "";
+    for (let user of this.table) {
+      allRowsTemaplate += this.#rowTemplateCreation(user); // верстку наполяем данными из обьекта 
+    }
+    domStructure
+      .querySelector("tbody")
+      .insertAdjacentHTML("afterbegin", allRowsTemaplate);
+    domStructure.addEventListener("click", this.#deleteRow);
+    this.elem = domStructure;
+  }
+
+  // Функция удления строки в которой была нажата кнопка.
+  #deleteRow(event) {
+    const target = event.target;
+    if (target.tagName == "BUTTON") {
+      target.closest("tr").remove();
+    }
+  }
+
+  #insertCoreStructureToElem() {
+    const domElement = document.createElement("table");
+    domElement.innerHTML = this.tableCoreStructure;
+    return domElement;
+  }
+
+  #rowTemplateCreation(user) {
+    return `<tr> 
+    <td>${user.name}</td>
+    <td>${user.age}</td>
+    <td>${user.salary}</td>
+    <td>${user.city}</td>
+    <td><button>X</button></td>
+    </tr>`;
   }
 }

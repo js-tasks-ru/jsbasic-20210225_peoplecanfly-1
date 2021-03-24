dc2da762e983864b5525525ca129bf860eb2510a
Yury Chernysh <yura388@yandex.ru>
2021-03-24 11:29:46 +0300

    1and2 tasks done

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,20 +1,15 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
-  tableCoreStructure = ` 
-  <thead>
+  // элемент дома который потом будет добавлен в html.body.
+
+  constructor(rows) {
+    this.users = rows;
+    this.elem = document.createElement("table");
+    this.#render(); // Функция  которая присваивает финальный ДОМ элемент this.elem
+  }
+
+  #headerTableTemplate() {
+    return `
+    <thead>
     <tr>
       <th>Имя</th>
       <th>Возраст</th>
@@ -22,51 +17,48 @@ export default class UserTable {
       <th>Город</th>
       <th></th>
     </tr>
-  </thead>
-  <tbody>
-  </tbody>`; // корневая структура таблицы
-  elem; // элемент дома который потом будет добавлен в html.body.
-
-  constructor(rows) {
-    this.table = rows;
-    this.#setDomElement(); // Функция  которая присваивает финальный ДОМ элемент this.elem
+    </thead>`;
   }
 
-  // Основная функция которая приваивает свойсту класса elem - готовый DOM элемент таблицы.
-  #setDomElement() {
-    const domStructure = this.#insertCoreStructureToElem();
-    let allRowsTemaplate = "";
-    for (let user of this.table) {
-      allRowsTemaplate += this.#rowTemplateCreation(user); // верстку наполяем данными из обьекта 
+  #rowsTableTemplate(users) {
+    let resultTemplate = "";
+    for (let user of users) {
+      resultTemplate += this.#rowTemplateCreation(user);
     }
-    domStructure
-      .querySelector("tbody")
-      .insertAdjacentHTML("afterbegin", allRowsTemaplate);
-    domStructure.addEventListener("click", this.#deleteRow);
-    this.elem = domStructure;
+    return resultTemplate;
+  }
+
+  #userTableTemplate({ users }) {
+    return `
+    ${this.#headerTableTemplate()}
+    <tbody>
+      ${this.#rowsTableTemplate(users)}
+    </tbody>
+    `;
+  }
+
+  #render() {
+    const template = this.#userTableTemplate({ users: this.users });
+    this.elem.insertAdjacentHTML("afterbegin", template);
+    this.elem.addEventListener("click", this.#deleteRow);
   }
 
   // Функция удления строки в которой была нажата кнопка.
   #deleteRow(event) {
     const target = event.target;
-    if (target.tagName == "BUTTON") {
+    console.log(target.dataset.action);
+    if (target.dataset.action == "remove") {
       target.closest("tr").remove();
     }
   }
 
-  #insertCoreStructureToElem() {
-    const domElement = document.createElement("table");
-    domElement.innerHTML = this.tableCoreStructure;
-    return domElement;
-  }
-
   #rowTemplateCreation(user) {
-    return `<tr> 
+    return `<tr > 
     <td>${user.name}</td>
     <td>${user.age}</td>
     <td>${user.salary}</td>
     <td>${user.city}</td>
-    <td><button>X</button></td>
+    <td><button data-action="remove">X</button></td>
     </tr>`;
   }
 }
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,18 +1,19 @@
 import createElement from "../../assets/lib/create-element.js";
 
 export default class ProductCard {
-  
   constructor(product) {
     this.product = product;
-    this.elem = document.createElement("div");
-    this.#insertDataToElem(this.elem);
+    this.elem = null;
+    this.#render();
   }
 
-  #insertDataToElem(element) {
-    element.classList.add("card");
-    element.insertAdjacentHTML("afterBegin", this.#insertProductData());
-    const button = element.querySelector(".card__button");
-    this.#addCustomEvent(button);
+  #render() {
+    this.elem = createElement(`
+    <div class="card">
+      ${this.#insertProductData()}
+    </div>`);
+    const button = this.elem.querySelector(".card__button");
+    button.addEventListener('click', this.#onCardButtonClick)
   }
 
   #insertProductData() {
@@ -31,19 +32,11 @@ export default class ProductCard {
     </div>`;
   }
 
-
-// тотальная кривизна тут наверно, но я с пользователскими ивентами долго разибирался, смог только так   
-  #addCustomEvent(button) {
-    // пришлось оборачивать в функцию так как не понял как можно это вызвать иначе в addEventListner
-    function createCustomEvent() {
-      const productIdDispatch = new CustomEvent("product-add", {
-        detail: this.product.id,
-        bubbles: true
-      });
-      button.dispatchEvent(productIdDispatch); // активация события ( я так это понял )
-    }
-
-    createCustomEvent = createCustomEvent.bind(this); // this будет button если не использовать bind
-    button.addEventListener("click", createCustomEvent); // тут уже просто вешаю обработчик 
+  #onCardButtonClick = () => {
+    const productIdDispatch = new CustomEvent("product-add", {
+      detail: this.product.id,
+      bubbles: true,
+    });
+    event.target.closest("button").dispatchEvent(productIdDispatch);
   }
 }

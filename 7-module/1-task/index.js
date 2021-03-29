22365c8472ce660d60c83f0b61d6ba74f71ff887
Yury Chernysh <yura388@yandex.ru>
2021-03-29 17:44:41 +0300

    1st and 2nd task DZ 7

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -1,7 +1,117 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class RibbonMenu {
+  #categories = null;
+  #ribbonInner = null;
+  #leftButton = null;
+  #rightButton = null;
+
   constructor(categories) {
-    this.categories = categories;
+    this.#categories = categories;
+    this.elem = null;
+    this.#render();
   }
+
+  #render() {
+    this.elem = createElement(` 
+      <div class="ribbon">
+        <button class="ribbon__arrow ribbon__arrow_left ribbon__arrow_visible">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </button>
+
+        <nav class="ribbon__inner">
+          ${this.#GetRibbonItemList()}
+        </nav>
+
+        <button class="ribbon__arrow ribbon__arrow_right">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </button>
+      </div>`);
+
+    this.#RibbonMoveElementsInit();
+    this.#moveRibbonMenu();
+    this.elem.addEventListener("click", this.#removeDefault); // лучше так или вызывать функцию внутри которыой уже будет прослушка события ? 
+    this.elem.addEventListener("click", this.#elementsStyleControl);
+    this.#addCustomListener();
+  }
+
+  #RibbonMoveElementsInit() {
+    this.#rightButton = this.elem.querySelector(".ribbon__arrow_right");
+    this.#leftButton = this.elem.querySelector(".ribbon__arrow_left");
+    this.#ribbonInner = this.elem.querySelector(".ribbon__inner");
+    this.#leftButton.classList.remove("ribbon__arrow_visible");
+    this.#rightButton.classList.add("ribbon__arrow_visible");
+  }
+
+  #GetRibbonItemList() {
+    return this.#categories.map((item) => this.#getRibbonItem(item)).join("");
+  }
+
+  #getRibbonItem(item) {
+    return `<a href="#" class="ribbon__item" data-id="${item.id}">${item.name}</a>`;
+  }
+
+  #moveRibbonMenu() {
+    this.#rightButton.addEventListener("click", () =>
+      this.#ribbonInner.scrollBy(350, 0)
+    );
+    this.#leftButton.addEventListener("click", () =>
+      this.#ribbonInner.scrollBy(-350, 0)
+    );
+    this.#ribbonInner.addEventListener("scroll", this.#buttonVisibilityCheck);
+  }
+
+  #buttonVisibilityCheck = () => {
+    let scrollLeft = this.#ribbonInner.scrollLeft;
+    let scrollWidth = this.#ribbonInner.scrollWidth;
+    let clientWidth = this.#ribbonInner.clientWidth;
+    let scrollRight = scrollWidth - scrollLeft - clientWidth;
+
+    if (scrollRight != 0) {
+      this.#rightButton.classList.add("ribbon__arrow_visible");
+    } else {
+      this.#rightButton.classList.remove("ribbon__arrow_visible");
+    }
+
+    if (scrollLeft != 0) {
+      this.#leftButton.classList.add("ribbon__arrow_visible");
+    } else {
+      this.#leftButton.classList.remove("ribbon__arrow_visible");
+    }
+  };
+
+  #removeDefault = (event) => {
+    if (event.target.tagName == "A") {
+      event.preventDefault();
+    }
+  };
+
+  #elementsStyleControl = (event) => {
+    if (event.target.classList.contains("ribbon__item")) {
+      let ribbonElement = this.#ribbonInner.querySelector(".ribbon__item_active");
+      // Не стал писать отдельную функцию для этого, а вынести этот IF от сюда нельзя так как тогда, при прокрутке слайдреа стили будут слетать. 
+      if (ribbonElement) {
+        ribbonElement.classList.remove("ribbon__item_active");
+      }
+      
+      event.target.classList.add("ribbon__item_active");
+    }
+  };
+
+  #addCustomListener() {
+    this.elem.addEventListener("click", this.#dsipatchCustomerEvent);
+  }
+
+  #dsipatchCustomerEvent = (event) => {
+    if (event.target.classList.contains("ribbon__item")) {
+      const ribbonItemId = event.target.dataset.id;
+
+      this.elem.dispatchEvent (
+        new CustomEvent('ribbon-select', { 
+        detail: ribbonItemId, 
+        bubbles: true 
+        })
+      )
+    }
+  };
 }
--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,6 +1,71 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class Modal {
   constructor() {
+    this.body = null;
+    this.title = null;
+    this.documentBody = document.querySelector("body");
   }
+
+  setTitle(title) {
+    this.title = title;
+  }
+
+  setBody(element) {
+    this.body = element.outerHTML;
+  }
+
+  open() {
+    this.documentBody.classList.add("modal-open");
+
+    let temlate = this.getTemplate();
+    this.documentBody.appendChild(createElement(temlate));
+    this.#addlisteners();
+  }
+
+  close() {
+    this.documentBody.classList.remove("modal-open");
+    this.documentBody.querySelector(".modal").remove();
+  }
+
+  getTemplate = () => {
+    return `<div class="modal">
+    <!--Прозрачная подложка перекрывающая интерфейс-->
+    <div class="modal__overlay"></div>
+
+    <div class="modal__inner">
+      <div class="modal__header">
+        <!--Кнопка закрытия модального окна-->
+        <button type="button" class="modal__close">
+          <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+        </button>
+
+        <h3 class="modal__title">
+          ${this.title}
+        </h3>
+      </div>
+
+      <div class="modal__body">
+        ${this.body}
+      </div>
+    </div>
+  </div>`;
+  };
+
+  #addlisteners = () => {
+    const button = this.documentBody.querySelector(".modal__close");
+
+    button.addEventListener("click", () => {
+      this.documentBody.querySelector(".modal").remove();
+      this.documentBody.classList.remove("modal-open");
+    });
+
+    this.documentBody.addEventListener('keydown', (event)=>{
+      if(event.code ==='Escape'){
+        this.documentBody.querySelector(".modal").remove();
+        this.documentBody.classList.remove("modal-open");
+      }
+    })
+
+  };
 }
